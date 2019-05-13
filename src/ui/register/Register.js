e790e9bdb2896b0eb041293c1fa7a5478cbd4ff7
AlexisLambois <lambois16@gmail.com>
2019-05-13 16:03:24 +0200

    add bootstrap and form register

--- src/ui/register/Register.js
+++ src/ui/register/Register.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import RegisterForm from "../../component/registerForm/RegisterForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Register extends React.Component {
   render() {
     return (
-      <div>
-        Hello boys
+      <div class="container">
+        <RegisterForm/>
       </div>
     )
   }
