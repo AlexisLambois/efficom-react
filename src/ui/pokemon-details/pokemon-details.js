0e284ed3dd75f880a97c39af8ad89551a6e6b150
Philippine <philippine_l@live.fr>
2019-05-15 10:28:15 +0200

    Added style on pokemonbyID

--- src/ui/pokemon-details/pokemon-details.js
+++ src/ui/pokemon-details/pokemon-details.js
@@ -31,23 +31,74 @@ export default class PokemonDetails extends React.Component {
         })
 
         return (
-            <div className="container">
-                <div className="pokemon-details">
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt=""/>
-                    <div className="details">
-                        <div className="header">
-                            <h1 className="mr-2">{pokemon.name}</h1>
-                            <div className="badges">
-                                {types}
-                            </div>
-                        </div>
-                        <div className="content">
-                            <p>Taille : {pokemon.height} m</p>
-                            <p>Poids : {pokemon.weight} kg</p>
-                            <p>{pokemon.description}</p>
-                        </div>
+            // <div className="container">
+            //     <div className="pokemon-details">
+            //         <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="" />
+            //         <div className="details">
+            //             <div className="header">
+            //                 <h1 className="mr-2">{pokemon.name}</h1>
+            //                 <div className="badges">
+            //                     {types}
+            //                 </div>
+            //             </div>
+            //             <div className="content">
+            //                 <p>Taille : {pokemon.height} m</p>
+            //                 <p>Poids : {pokemon.weight} kg</p>
+            //                 <p>{pokemon.description}</p>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
+
+            <div id="pokedex">
+                <div class="sensor">
+                    <button></button>
+                </div>
+                <div class="camera-display">
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
+                </div>
+                <div class="divider"></div>
+                <div class="stats-display">
+                <div className="container">
+                    <div className="row">
+                        <h2>{pokemon.name}</h2>
+                        <span className="badges">
+                            {types}
+                        </span>
+                    </div>
+
+                </div>
+                    <p>Taille : {pokemon.height} m</p>
+                    <p>Poids : {pokemon.weight} kg</p>
+                    <p>{pokemon.description}</p>
+
+                </div>
+                <div class="botom-actions">
+                    <div id="actions">
+                        <button class="a"></button>
+                    </div>
+                    <div id="cross">
+                        <button class="cross-button up"></button>
+                        <button class="cross-button right"></button>
+                        <button class="cross-button down"></button>
+                        <button class="cross-button left"></button>
+                        <div class="cross-button center"> </div>
                     </div>
                 </div>
+                <div class="input-pad"><input /></div>
+
+                <div class="bottom-modes">
+
+                    <button class="level-button"></button>
+                    <button class="level-button"></button>
+                    <button class="level-button"></button>
+                    <button class="level-button"></button>
+
+                    <button class="pokedex-mode black-button">Pokedex</button>
+                    <button class="game-mode black-button">Game</button>
+
+                </div>
+
             </div>
         )
     }
